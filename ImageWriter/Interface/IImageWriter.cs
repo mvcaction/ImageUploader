472907b560e107326251456dae7e4245afd2184d
hessam ab <mvcaction@hotmail.com>
2019-09-25 13:27:26 +0330

    Add project files.

---
+++ ImageWriter/Interface/IImageWriter.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ImageWriter.Interface
+{
+    public interface IImageWriter
+    {
+        Task<string> UploadImage(IFormFile file);
+    }
+}
